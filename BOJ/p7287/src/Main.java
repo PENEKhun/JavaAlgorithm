7afdc5fedc8210f8f6468a76f5d3c8f835f54960
penekhun <penekhun@gmail.com>
2023-06-30 18:03:12 +0900

    boj 7287

---
+++ BOJ/p7287/src/Main.java
@@ -0,0 +1,17 @@
+
+/*
+문제
+자신이 백준 온라인 저지(BOJ)에서 맞은 문제의 수와 아이디를 그대로 출력하는 프로그램을 작성하시오.
+
+입력
+이 문제는 입력이 없다.
+
+출력
+첫 줄에 자신이 맞은 문제의 수, 둘째 줄에 아이디를 출력한다.
+ */
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("17\npenekhun");
+  }
+}
+
