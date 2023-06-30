37dddf5244f1dbfd6dbda7af34c0c105c03aa4ab
penekhun <penekhun@gmail.com>
2023-06-30 18:02:47 +0900

    boj 2741

---
+++ BOJ/p2741/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+
+        for (int i = 1; i <= a; i++){
+            System.out.println(i);
+        }
+    }
+}
