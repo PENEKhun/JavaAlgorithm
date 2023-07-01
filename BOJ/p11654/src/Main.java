51d057d5d196b3cc780381fd6348db1e8068f10f
penekhun <penekhun@gmail.com>
2023-07-02 00:38:49 +0900

    solved BAEKJOON #11654 아스키 코드

---
+++ BOJ/p11654/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/*
+    성훈ㅇㅏ.... 화이팅하자 ....
+    solved BAEKJOON #11654 아스키 코드
+    url : https://www.acmicpc.net/problem/11654
+*/
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner s = new Scanner(System.in);
+      String str = s.nextLine();
+      System.out.println((int) str.charAt(0));
+  }
+}
