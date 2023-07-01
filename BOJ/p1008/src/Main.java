6a7952d5d7085f3732c4048781b5879030fd3a89
penekhun <penekhun@gmail.com>
2023-07-01 17:04:45 +0900

    solved BAEKJOON #1008 A/B

---
+++ BOJ/p1008/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/*
+    성훈ㅇㅏ.... 화이팅하자 ....
+    sovled BAEKJOON #1008 A/B
+    url : https://www.acmicpc.net/problem/1008
+*/
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner s = new Scanner(System.in);
+      int a = s.nextInt(), b = s.nextInt();
+      double result = (double) a / b;
+
+      System.out.println(result);
+  }
+}
