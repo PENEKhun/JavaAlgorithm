e02534670f7524cfc3f6d7365db7e0d8d76bcfef
penekhun <penekhun@gmail.com>
2023-07-01 16:34:26 +0900

    BAEKJOON - 10951 번- A+B - 4

---
+++ BOJ/p10951/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/*
+    성훈ㅇㅏ.... 화이팅하자 ....
+    BAEKJOON - 10951 번- A+B - 4
+    url : https://www.acmicpc.net/problem/10951
+*/
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner s = new Scanner(System.in);
+
+      while(s.hasNextInt()){
+        System.out.println(s.nextInt() + s.nextInt());
+      }
+  }
+}
