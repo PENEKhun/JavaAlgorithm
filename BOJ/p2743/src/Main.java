3e776492fde773709c3e21a1b68cc5f251b0deb0
penekhun <penekhun@gmail.com>
2023-07-04 20:35:26 +0900

    solved BAEKJOON #2743 단어 길이 재기

---
+++ BOJ/p2743/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/*
+    성훈ㅇㅏ.... 화이팅하자 ....
+    solved BAEKJOON #2743 단어 길이 재기
+    url : https://www.acmicpc.net/problem/2743
+*/
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner s = new Scanner(System.in);
+      String str = s.nextLine();
+
+      System.out.println(str.length());
+  }
+}
