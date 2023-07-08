0c7eb6fc6abd3d2f976823ae2fb88104edd83b3d
penekhun <penekhun@gmail.com>
2023-07-08 21:52:27 +0900

    solved BAEKJOON #18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ BOJ/p18108/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/*
+    성훈ㅇㅏ.... 화이팅하자 ....
+    solved BAEKJOON #18108 1998년생인 내가 태국에서는 2541년생?!
+    url : https://www.acmicpc.net/problem/18108
+*/
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner s = new Scanner(System.in);
+        System.out.println(s.nextInt()-543);
+  }
+}
