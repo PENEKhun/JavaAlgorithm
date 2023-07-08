432c578380fc7b832c587c4a59b5baec23cbff79
penekhun <penekhun@gmail.com>
2023-07-08 22:18:03 +0900

    solved BAEKJOON #25314 코딩은 체육과목 입니다

---
+++ BOJ/p25314/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+/*
+    성훈ㅇㅏ.... 화이팅하자 ....
+    solved BAEKJOON #25314 코딩은 체육과목 입니다
+    url : https://www.acmicpc.net/problem/25314
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.println("long ".repeat(n/4) + "int");
+    }
+}
