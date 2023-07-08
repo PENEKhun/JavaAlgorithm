d0a294064e851006c3a246f5a94468d2ed16e74e
penekhun <penekhun@gmail.com>
2023-07-08 21:52:21 +0900

    solved BAEKJOON #10926 ??!

---
+++ BOJ/p10926/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/*
+    성훈ㅇㅏ.... 화이팅하자 ....
+    solved BAEKJOON #10926 ??!
+    url : https://www.acmicpc.net/problem/10926
+*/
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner s = new Scanner(System.in);
+        System.out.println(s.nextLine() + "??!");
+  }
+}
