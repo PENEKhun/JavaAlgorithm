70dc93ba644a7bbf05ccba022e5d67cde0a524bf
penekhun <penekhun@gmail.com>
2023-09-17 20:09:41 +0900

    first Commit

---
+++ inflearn/자바-알고리즘-문제풀이-입문- 코딩테스트-대비/1. 문자 찾기/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    String str = s.next();
+    int cnt = 0;
+
+
+  }
+}
