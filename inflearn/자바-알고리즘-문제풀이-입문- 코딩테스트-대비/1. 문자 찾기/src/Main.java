d7d264f55118212e5fda2c70e91fe91df167a1b8
penekhun <penekhun@gmail.com>
2023-09-17 20:25:33 +0900

    solve

--- inflearn/자바-알고리즘-문제풀이-입문- 코딩테스트-대비/1. 문자 찾기/src/Main.java
+++ inflearn/자바-알고리즘-문제풀이-입문- 코딩테스트-대비/1. 문자 찾기/src/Main.java
@@ -3,9 +3,17 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args){
     Scanner s = new Scanner(System.in);
-    String str = s.next();
+    String str = s.next().toLowerCase();
+    String toFind = s.next().toLowerCase();
+
     int cnt = 0;
 
+    for (char c : str.toCharArray()) {
+      if (c == toFind.charAt(0)){
+        cnt++;
+      }
+    }
 
+    System.out.println(cnt);
   }
 }
