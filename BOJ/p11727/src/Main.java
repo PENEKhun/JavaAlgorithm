da727a1b3c006df37b8b04de3d845e05d8246cc9
penekhun <penekhun@gmail.com>
2023-12-11 15:39:42 +0900

    solved BAEKJOON #11727 2×n 타일링
    샘플 케이스 계산 정확하게 해서... 점화식 잘 새우기.

---
+++ BOJ/p11727/src/Main.java
@@ -0,0 +1,38 @@
+import static java.math.BigInteger.ONE;
+import static java.math.BigInteger.TWO;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+/*
+    성훈ㅇㅏ.... 화이팅하자 ....
+    solved BAEKJOON #11727 2×n 타일링
+    url : https://www.acmicpc.net/problem/11727
+*/
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+
+    BigInteger[] memo = new BigInteger[n + 5];
+    memo[1] = ONE;
+    memo[2] = BigInteger.valueOf(3L);
+
+    for (int i = 3; i <= n; i++) {
+      memo[i] = (memo[i - 2].multiply(TWO).add(memo[i - 1]));
+    }
+
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write((memo[n].divideAndRemainder(BigInteger.valueOf(10_007)))[1] + "\n");
+    bw.flush();
+
+    bw.close();
+    br.close();
+  }
+}
