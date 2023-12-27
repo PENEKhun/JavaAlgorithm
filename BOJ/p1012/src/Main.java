e4fc18d7d3d1d9faa17de277c50d3f7e54387de9
penekhun <penekhun@gmail.com>
2023-12-27 23:37:00 +0900

    Update Main.java
    뭐지 깃엔 올라가있는데 제출을 안했나..?
    그래서 다시 풀었네..

--- BOJ/p1012/src/Main.java
+++ BOJ/p1012/src/Main.java
@@ -3,79 +3,78 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 /*
-    성훈ㅇㅏ.... 화이팅하자 ....
-    solved BAEKJOON #1012 유기농 배추
-    url : https://www.acmicpc.net/problem/1012
-
-    아니 이것도 기본으로 준 케이스가 이해가 안됨
-    -> 지용이가 문제 케이스 이해시켜줬음 땡큐 @camp5803
+    BAEKJOON #1012 유기농 배추
+    https://www.acmicpc.net/problem/1012
 */
 
 public class Main {
 
-  static boolean[][] baechu;
+  static boolean[][] bat;
   static boolean[][] visited;
-  static int[] dx = {0, 0, -1, 1, 0};
-  static int[] dy = {1, -1, 0, 0, 0};
-  static int width;
-  static int height;
+  static int[] dx = {1, -1, 0, 0};
+  static int[] dy = {0, 0, 1, -1};
+
+  static void dfs(int sero, int garo) {
+    bat[sero][garo] = true;
+    visited[sero][garo] = true;
+
+    for (int i = 0; i < 4; i++) {
+
+      int newSero = dy[i] + sero;
+      int newGaro = dx[i] + garo;
+
+      if (newSero < 0 || newSero > bat.length - 1) {
+        continue;
+      }
+
+      if (newGaro < 0 || newGaro > bat[0].length - 1) {
+        continue;
+      }
+
+      if (!visited[newSero][newGaro] && bat[newSero][newGaro]) {
+        dfs(newSero, newGaro);
+      }
+    }
+  }
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int testCase = Integer.parseInt(br.readLine());
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    for (int t = 0; t < testCase; t++) {
-      int result = 0;
-      String[] input = br.readLine().split(" ");
-      width = Integer.parseInt(input[0]);
-      height = Integer.parseInt(input[1]);
-      int howManyPosition = Integer.parseInt(input[2]);
+    for (int i = 0; i < testCase; i++) {
+      int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+      int garo = input[0];
+      int sero = input[1];
+      int baechu = input[2];
 
-      baechu = new boolean[height][width];
-      visited = new boolean[height][width];
-      for (int i = 0; i < howManyPosition; i++) {
-        String[] position = br.readLine().split(" ");
-        int x = Integer.parseInt(position[0]);
-        int y = Integer.parseInt(position[1]);
-        baechu[y][x] = true;
+      bat = new boolean[sero][garo];
+      visited = new boolean[sero][garo];
+
+      // init baechu
+      for (int i1 = 0; i1 < baechu; i1++) {
+        int[] bachuPosition = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt)
+            .toArray();
+        bat[bachuPosition[1]][bachuPosition[0]] = true;
       }
 
-      for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-          if (!visited[y][x] && baechu[y][x]) {
-            visited[y][x] = true;
-            bfs(x, y);
-            result++;
+      int count = 0;
+      for (int row = 0; row < sero; row++) {
+        for (int len = 0; len < garo; len++) {
+          if (!visited[row][len] && bat[row][len]) {
+            count++;
+            dfs(row, len);
           }
         }
       }
-      bw.write(result + "\n");
+
+      bw.write(count + "\n");
     }
 
     bw.flush();
     bw.close();
     br.close();
-  }
-
-  private static void bfs(int x, int y) {
-    for (int i = 0; i < 4; i++) {
-      int newX = x + dx[i];
-      int newY = y + dy[i];
-
-      if (newX < 0 || newY < 0) {
-        continue;
-      }
-
-      if (newX == width || newY == height) {
-        continue;
-      }
-
-      if (!visited[newY][newX] && baechu[newY][newX]) {
-        visited[newY][newX] = true;
-        bfs(newX, newY);
-      }
-    }
   }
 }
