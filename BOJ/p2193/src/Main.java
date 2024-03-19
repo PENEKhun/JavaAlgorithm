cc75fdc0035b20fe60bb00db5694917e06073265
penekhun <penekhun@gmail.com>
2024-03-19 18:02:23 +0900

    BAEKJOON 2193 이친수

---
+++ BOJ/p2193/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+  BAEKJOON 2193 이친수
+  https://www.acmicpc.net/problem/2193
+*/
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    long[] dp = new long[n + 1];
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    System.out.println(dp[n]);
+  }
+}
