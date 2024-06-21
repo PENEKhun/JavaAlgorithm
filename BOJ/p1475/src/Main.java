9428029f8d5433cb379b33418b357858d8a539ce
penekhun <penekhun@gmail.com>
2024-06-21 12:39:26 +0900

    BAEKJOON 1475 방 번호

---
+++ BOJ/p1475/src/Main.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*
+  BAEKJOON 1475 방 번호
+  https://www.acmicpc.net/problem/1475
+*/
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] n = br.readLine().split("");
+    boolean[][] set = new boolean[1_000_001][10];
+    int result = 1;
+    for (int j = 0; j < n.length; j++) {
+      String num = n[j];
+      int number = Integer.parseInt(num);
+      boolean processed = false;
+      for (int i = 0; i < result; i++) {
+        if (!set[i][number]) {
+          set[i][number] = true;
+          processed = true;
+          break;
+        } else if (number == 6 || number == 9) {
+          if (!set[i][6]) {
+            set[i][6] = true;
+            processed = true;
+            break;
+          }
+
+          if (!set[i][9]) {
+            set[i][9] = true;
+            processed = true;
+            break;
+          }
+        }
+      }
+      if (!processed) {
+        result++;
+        j--;
+      }
+    }
+
+    System.out.println(result);
+  }
+}
---
+++ BOJ/p1475/src/TestHelper.java
@@ -0,0 +1,193 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 이 테스트 코드는 <a href="https://github.com/PENEKhun/Baekjoon-java-starter">Baekjoon-java-starter</a>를 사용하여
+ * 생성되었습니다.
+ *
+ * @author PENEKhun
+ */
+public class TestHelper {
+
+  private static final HashMap<Field, Object> initialStates = new HashMap<>();
+
+  public static void main(String[] args) {
+    captureInitialState();
+
+    TestCase[] testCases = new TestCase[] {
+        new TestCase(
+// input
+"""
+9999
+""",
+// output
+"""
+2
+"""),
+new TestCase(
+// input
+"""
+122
+""",
+// output
+"""
+2
+"""),
+new TestCase(
+// input
+"""
+12635
+""",
+// output
+"""
+1
+"""),
+new TestCase(
+// input
+"""
+888888
+""",
+// output
+"""
+6
+"""),
+
+    };
+
+    int passedCases = 0;
+
+    for (int i = 0; i < testCases.length; i++) {
+      resetToInitialState();
+      TestCase testCase = testCases[i];
+
+      ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+      PrintStream printStream = new PrintStream(outputStream);
+      PrintStream printOut = System.out;
+      System.setOut(printStream);
+      System.setIn(new ByteArrayInputStream(testCase.input.getBytes()));
+
+      try {
+        Main.main(new String[0]);
+      } catch (Exception e) {
+        System.setOut(printOut);
+        printFail(i + 1, testCase, "Exception 발생");
+        e.printStackTrace();
+      }
+
+      String output = outputStream.toString().stripTrailing();
+      System.setOut(printOut);
+      if (output.equals(testCase.expectedOutput.stripTrailing())) {
+        passedCases++;
+        continue;
+      } else {
+        printFail(i + 1, testCase,
+            red("""
+                [실제 값]
+                %s
+                """.formatted(output)));
+      }
+    }
+
+    System.out.println("테스트 완료 (" + passedCases + " / " + testCases.length + ")");
+    if (passedCases == testCases.length) {
+      System.out.println("주어진 케이스에 대해 잘 동작하고 있습니다.");
+    }
+  }
+
+  private static void captureInitialState() {
+    try {
+      Class<?> clazz = Main.class;
+      var clonedClass = clazz.getConstructor().newInstance();
+      Field[] fields = clonedClass.getClass().getDeclaredFields();
+      System.out.println(Arrays.toString(fields));
+
+      for (Field field : fields) {
+        if (Modifier.isStatic(field.getModifiers())) {
+          field.setAccessible(true);
+          Object fieldValue = field.get(null);
+          try {
+            byte[] serializedField = SerializationUtils.serialize(fieldValue);
+            initialStates.put(field, serializedField);
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    } catch (Exception e) {
+      System.out.println(red("Main 클래스에 접근할 수 없습니다."));
+    }
+  }
+
+  private static void resetToInitialState() {
+    try {
+      for (Map.Entry<Field, Object> entry : initialStates.entrySet()) {
+        Field field = entry.getKey();
+        byte[] serializedState = (byte[]) entry.getValue();
+        try {
+          Object originalState = SerializationUtils.deserialize(serializedState);
+          field.setAccessible(true);
+          field.set(null, originalState);
+        } catch (IOException | ClassNotFoundException e) {
+          e.printStackTrace(); // Or handle more gracefully
+        }
+      }
+
+    } catch (IllegalAccessException e) {
+      System.out.println(red("Main 클래스에 접근할 수 없습니다."));
+    }
+  }
+
+  private static void printFail(int caseNumber, TestCase testCase, String message) {
+    System.out.printf("""
+        ====== %s ======
+        [입력 값]
+        %s
+        [기대 값]
+        %s
+        """, red(caseNumber + " 번째 케이스 실패"), testCase.input, testCase.expectedOutput);
+
+    System.out.println(message);
+  }
+
+  private static String red(String message) {
+    return "\u001B[31m%s\u001B[0m".formatted(message);
+  }
+
+  private static class TestCase {
+
+    public String input;
+    public String expectedOutput;
+
+    public TestCase(String input, String expectedOutput) {
+      this.input = input;
+      this.expectedOutput = expectedOutput;
+    }
+  }
+
+  public static class SerializationUtils {
+
+    public static byte[] serialize(Object obj) throws IOException {
+      try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+          ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+        oos.writeObject(obj);
+        return bos.toByteArray();
+      }
+    }
+
+    public static Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+      try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+          ObjectInputStream ois = new ObjectInputStream(bis)) {
+        return ois.readObject();
+      }
+    }
+  }
+}
