45e2a4981c4249dae9c6c747ab47a4193b685ac2
penekhun <penekhun@gmail.com>
2024-07-23 19:06:39 +0900

    Optimize

--- BOJ/p12018/src/Main.java
+++ BOJ/p12018/src/Main.java
@@ -13,11 +13,9 @@ public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String[] NM = br.readLine().split(" ");
-    int n = Integer.parseInt(NM[0]); // 과목수
-    int m = Integer.parseInt(NM[1]); // 성준이의 마일리지
-    int[][] score = new int[n][]; // 과목별 마일리지 얼마나
+    int n = Integer.parseInt(NM[0]);
+    int m = Integer.parseInt(NM[1]);
 
-    // 사용해야하는 최소 마일리지
     int[] resultTable = new int[n];
     for (int i = 0; i < n; i++) {
       String[] input = br.readLine().split(" ");
@@ -25,7 +23,6 @@ public class Main {
       int[] temp = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt)
           .sorted()
           .toArray();
-      Arrays.sort(temp);
       int submitted = Integer.parseInt(input[0]);
       int maxMember = Integer.parseInt(input[1]);
       if (maxMember > submitted) {
