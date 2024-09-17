d30a36074350368111244f58a6552f8bce9c175b
penekhun <penekhun@gmail.com>
2024-09-18 07:17:05 +0900

    BAEKJOON 14699 관악산 등산
    2안 (더 구리게 만들기 ㅋㅅㅋ)

--- BOJ/p14699/src/Main.java
+++ BOJ/p14699/src/Main.java
@@ -3,9 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.List;
 
 /*
   BAEKJOON 14699 관악산 등산
@@ -29,17 +27,20 @@ public class Main {
     int N = Integer.parseInt(inputs[0]); // 쉼터의 수
     int M = Integer.parseInt(inputs[1]);  // 쉼터 오가는 수
     int[] heights = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-    ArrayList<Mountain> mountains = new ArrayList<>();
-    Map<Integer, ArrayList<Mountain>> roads = new HashMap<>();
-    for (int i = 1; i <= N; i++) {
-      roads.put(i, new ArrayList<>());
-      mountains.add(new Mountain(i, heights[i - 1]));
+    boolean[][] hasRoute = new boolean[N + 1][N + 1];
+    List<Mountain> mountains = new ArrayList<>();
+
+    for (int i = 0; i < heights.length; i++) {
+      mountains.add(new Mountain(i + 1, heights[i]));
     }
 
     for (int i = 0; i < M; i++) {
       int[] road = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-      roads.get(road[0]).add(new Mountain(road[1], heights[road[1] - 1]));
-      roads.get(road[1]).add(new Mountain(road[0], heights[road[0] - 1]));
+      if (heights[road[0] - 1] < heights[road[1] - 1]) {
+        hasRoute[road[0]][road[1]] = true;
+      } else if (heights[road[0] - 1] > heights[road[1] - 1]) {
+        hasRoute[road[1]][road[0]] = true;
+      }
     }
 
     int[] dp = new int[N + 1];
@@ -47,17 +48,18 @@ public class Main {
     mountains.sort((o1, o2) -> Integer.compare(o2.height, o1.height));
     for (int i = 0; i < mountains.size(); i++) {
       Mountain nowMountain = mountains.get(i);
-      int nowNumber = nowMountain.number;
-      int nowHeight = nowMountain.height;
       int max = -1;
-      for (Mountain nextMountain : roads.get(nowNumber)) {
-        if (nowHeight < nextMountain.height) {
-          max = Math.max(max, dp[nextMountain.number]);
+      for (int next = 1; next <= N; next++) {
+        if (nowMountain.number == next)
+          continue;
+
+        if (hasRoute[nowMountain.number][next]) {
+          max = Math.max(max, dp[next]);
         }
       }
 
       if (max != -1) {
-        dp[nowNumber] += max;
+        dp[nowMountain.number] += max;
       }
     }
 
