ea249bb2402a33fd04d43d017368e0ee33be46b5
penekhun <penekhun@gmail.com>
2024-10-08 15:04:09 +0900

    프로그래머스 모의고사
    수정핑~

--- programmers/challenges/완전탐색/모의고사/src/Solution.java
+++ programmers/challenges/완전탐색/모의고사/src/Solution.java
@@ -2,61 +2,25 @@ import java.util.*;
 
 class Solution {
   public int[] solution(int[] answers) {
-    int[] correct = new int[] {0, 0, 0};
+    int[] correct = {0, 0, 0};
+    int[] one = {1, 2, 3, 4, 5};
+    int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
+    int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
     int ansLen = answers.length;
-    int one = 1;
-    int twoAns = 1;
-    int twoIdx = 0;
-    int threeIdx = 0;
-    int threeAns = 3;
     int max = 0;
     for (int i=0; i<ansLen; i++) {
       int nowAns = answers[i];
-      if (nowAns == one) {
+      if (nowAns == one[i % one.length]) {
         correct[0]++;
       }
 
-      one++;
-      if (one == 6) {
-        one = 1;
+      if (nowAns == two[i % two.length]) {
+        correct[1]++;
       }
 
-      if (twoIdx % 2 == 0) {
-        if (nowAns == 2) {
-          correct[1]++;
-        }
-      } else {
-        if (nowAns == twoAns) {
-          correct[1]++;
-        }
-        twoAns++;
-        if (twoAns == 2) {
-          twoAns++;
-        }
-        if (twoAns == 6) {
-          twoAns = 1;
-        }
-      }
-      twoIdx++;
-
-      // 33, 11, 22, 44, 55
-      if (threeAns == nowAns) {
+      if (nowAns == three[i % three.length]) {
         correct[2]++;
-      }
-      threeIdx++;
-      if (threeIdx == 2) {
-        if (threeAns == 3) {
-          threeAns = 1;
-        } else if (threeAns == 1) {
-          threeAns = 2;
-        } else if (threeAns == 2) {
-          threeAns = 4;
-        } else if (threeAns == 4) {
-          threeAns = 5;
-        } else if (threeAns == 5) {
-          threeAns = 3;
-        }
-        threeIdx = 0;
       }
 
       max = Math.max(max, correct[0]);
