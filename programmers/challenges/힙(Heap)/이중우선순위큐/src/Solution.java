cdca5db6929e36bc9483ca29a3dd3852896bc797
penekhun <penekhun@gmail.com>
2024-10-19 16:29:46 +0900

    프로그래머스 이중우선순위큐
    개선개선

--- programmers/challenges/힙(Heap)/이중우선순위큐/src/Solution.java
+++ programmers/challenges/힙(Heap)/이중우선순위큐/src/Solution.java
@@ -2,40 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        Queue<Integer> maxQ = new PriorityQueue<>((o1, o2) -> {
-            return o2 - o1;
-        });
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         
-        Queue<Integer> minQ = new PriorityQueue<>((o1, o2) -> {
-            return o1 - o2;
-        });
-        
-        
-        for (String operate : operations) {
-            String[] command = operate.split(" ");
-            int operation = Integer.parseInt(command[1]);
+        for (String operation : operations) {
+            String[] command = operation.split(" ");
+            int num = Integer.parseInt(command[1]);
+            
             if (command[0].equals("I")) {
-                maxQ.add(operation);
-                minQ.add(operation);
-            } else if (command[0].equals("D")) {
-                if (operation == 1) {
-                    // 최대 삭제
-                    if (!maxQ.isEmpty()) {
-                        minQ.remove(maxQ.poll());
-                    }
-                } else if (operation == -1){
-                    // 최소 삭제
-                    if (!minQ.isEmpty()) { 
-                        maxQ.remove(minQ.poll());
-                    }
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            } else if (!map.isEmpty()) {
+                int removeKey = num == 1 ? map.lastKey() : map.firstKey();
+                if (map.get(removeKey) == 1) {
+                    map.remove(removeKey);
+                } else {
+                    map.put(removeKey, map.get(removeKey) - 1);
                 }
             }
         }
         
-        int[] ans = {0, 0};
-        if (!maxQ.isEmpty()) ans[0] = maxQ.peek(); else ans[0] = 0;
-        if (!minQ.isEmpty()) ans[1] = minQ.peek(); else ans[1] = 0;
-        
-        return ans;
+        if (map.isEmpty()) {
+            return new int[]{0, 0};
+        } else {
+            return new int[]{map.lastKey(), map.firstKey()};
+        }
     }
 }
