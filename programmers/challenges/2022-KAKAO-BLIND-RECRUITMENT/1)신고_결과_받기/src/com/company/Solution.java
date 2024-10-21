3a48cc1d6b5071e18189a6fc0186a4c89ab0a453
penekhun <penekhun@gmail.com>
2024-10-22 02:13:45 +0900

    프로그래머스 신고 결과 받기
    re

--- programmers/challenges/2022-KAKAO-BLIND-RECRUITMENT/1)신고_결과_받기/src/com/company/Solution.java
+++ programmers/challenges/2022-KAKAO-BLIND-RECRUITMENT/1)신고_결과_받기/src/com/company/Solution.java
@@ -1,54 +1,35 @@
-package com.company;
-
 import java.util.*;
 
 class Solution {
-    public static int[] solution(String[] id_list, String[] report, int k) {
+    public int[] solution(String[] id_list /* ID 목록 */, String[] reports /*신고*/, int k /*정지 기준*/) {
+        Map<String, Set<String>> reportBy = new HashMap<>(); // Key를 신고한 사람 목록
+        Map<String, Integer> mailCnt = new HashMap<>();
 
-        HashSet<String> idList = new HashSet<>(List.of(id_list));
+        for (String username : id_list) {
+            reportBy.put(username, new HashSet<>());
+        }
 
-        // 각 회원 별 신고 현황
-        HashMap<String, HashSet<String>> reportMap = new HashMap<>();
-        // 각 회원별 누적 경고 횟수
-        HashMap<String, Integer> reportedMap = new HashMap<>();
-        // 정지된 회원
-        HashSet<String> overReportedList = new HashSet<>();
-        // 신고 리스트(중복 제외)
-        HashSet<String> reportWithoutDuplicate = new HashSet<>(List.of(report));
+        for (String report : reports) {
+            String user = report.split(" ")[0];
+            String reported = report.split(" ")[1];
+            reportBy.get(reported).add(user);
+        }
 
-        for (String s : reportWithoutDuplicate) {
-            String reporter = s.split(" ")[0];
-            String reported = s.split(" ")[1];
-
-            if (!idList.contains(reported) || !idList.contains(reporter))
-                continue;
-
-            HashSet<String> reportedList;
-            if (reportMap.get(reporter) == null) reportedList = new HashSet<>();
-            else reportedList = reportMap.get(reporter);
-
-            Integer reportedCount = reportedMap.get(reported);
-
-            reportedList.add(reported);
-            if (reportedCount == null)
-                reportedMap.put(reported, 1);
-            else {
-                if (reportedCount + 1 >= k) {
-                    overReportedList.add(reported);
+        int[] ans = new int[id_list.length];
+        for (int i=0; i<id_list.length; i++) {
+            String reported = id_list[i];
+            if (reportBy.get(reported).size() >= k) {
+                // 신고자들
+                for (String reporter : reportBy.get(reported)) {
+                    mailCnt.put(reporter, mailCnt.getOrDefault(reporter, 0) + 1);
                 }
-                reportedMap.put(reported, reportedCount + 1);
             }
-            reportMap.put(reporter, reportedList);
         }
 
-        int i = 0;
-        int[] answer = new int[idList.size()];
-        for (Map.Entry<String, HashSet<String>> entry : reportMap.entrySet()) {
-            HashSet<String> value = entry.getValue();
-            value.retainAll(overReportedList);
-            answer[i] = value.size();
-            i++;
+        for (int i=0; i<id_list.length; i++) {
+            String user = id_list[i];
+            ans[i] = mailCnt.getOrDefault(user, 0);
         }
-        return answer;
+        return ans;
     }
 }
