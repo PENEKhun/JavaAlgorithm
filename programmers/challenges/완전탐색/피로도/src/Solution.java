5424aa230a58080b9b1b5bb6cc0edf9f5bc2ca1d
penekhun <penekhun@gmail.com>
2024-10-31 18:30:20 +0900

    프로그래머스 피로도
    222

--- programmers/challenges/완전탐색/피로도/src/Solution.java
+++ programmers/challenges/완전탐색/피로도/src/Solution.java
@@ -1,61 +1,22 @@
-import java.util.*;
-
 class Solution {
-    int[][] dungeons;
-    int k;
-    int max = 0;
-    
-    public int solution(int initK, int[][] temp) {
-        int answer = -1;
-        dungeons = temp;
-        k = initK;
-        int n = temp.length;
-        getCombinations(n, 0, 0, new Integer[n + 1], new boolean[n]);
-        return max;
+    public static boolean check[];
+    public static int ans = 0;
+
+    public int solution(int k, int[][] dungeons) {
+        check = new boolean[dungeons.length];
+
+        dfs(k, dungeons, 0);
+
+        return ans;
     }
-    
-    private void getCombinations(int n, int depth, int now, Integer[] route, boolean[] visited) {
-        if (depth == n) {
-            // 계산
-            calc(route, k);
-            return;
-        }
-        
-        for (int i=0; i<n; i++) {
-            if (visited[i]) continue;
-            
-            // 안감
-            visited[i] = true;
-            getCombinations(n, depth + 1, now, route, visited);
-            route[now] = null;
-            visited[i] = false;
-            
-            // 감
-            visited[i] = true;
-            route[now] = i;
-            getCombinations(n, depth + 1, now + 1, route, visited);
-            route[now+1] = null;
-            visited[i] = false;
-        }
-    }
-    
-    private void calc(Integer[] routes, int tired) {
-        // System.out.println(Arrays.toString(routes));
-        int visited = 0;
-        for (Integer route : routes) {
-            if (route == null) continue;
-            int required = dungeons[route][0];
-            int minus = dungeons[route][1];
-            
-            if (required > tired) {
-                return;
+    public static void dfs(int tired, int[][] dungeons, int cnt){
+        for(int i=0; i<dungeons.length; i++){
+            if(!check[i] && dungeons[i][0]<=tired){
+                check[i] = true;
+                dfs(tired-dungeons[i][1], dungeons, cnt+1);
+                check[i] = false;
             }
-            
-            visited++;
-            tired -= minus;
         }
-        
-        max = Math.max(max, visited);
-        return;
+        ans = Math.max(ans, cnt);
     }
 }
